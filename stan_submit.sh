1a60b58e4a175061b12e6e80259888c9fbc4abc8
Mattias Cross <mcross2@sheffield.ac.uk>
2026-06-11 12:16:54 +0100

    prep biggpu exp

--- stan_submit.sh
+++ stan_submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=styletts2
 #SBATCH --time=96:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=80G
+#SBATCH --mem=128G
 #SBATCH --output=Models/logs/styletts2.out
 #SBATCH --partition=gpu,gpu-h100,gpu-h100-nvl
 #SBATCH --qos=gpu
@@ -15,8 +15,8 @@ ml CUDA/12.4.0
 
 source .venv/bin/activate
 
-if [[ "$s" == *cde* ]]; then
-    python train_finetune_cde.py -p $1
+if [[ "$2" == cde ]]; then
+  accelerate launch --mixed_precision=fp16 --num_processes=1 train_finetune_cde_accelerate.py --config_path $1
 else
-    python train_finetune.py -p $1
+  accelerate launch --mixed_precision=fp16 --num_processes=1 train_finetune_accelerate.py --config_path $1
 fi
